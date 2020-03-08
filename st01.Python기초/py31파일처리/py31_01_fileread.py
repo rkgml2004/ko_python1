b105545b2376de7ff44be991a2e4f1a4141d4a25
ko <ko@naver.com>
2020-03-08 18:04:50 +0900

    rkgml2004

--- st01.Python기초/py31파일처리/py31_01_fileread.py
+++ st01.Python기초/py31파일처리/py31_01_fileread.py
@@ -1,5 +1,5 @@
 
-#readline() 파일에서 한 줄씩 읽기
+# readline() 파일에서 한 줄씩 읽기
 #########################
 print("readline() 파일에서 한 줄씩 읽기")
 pfr = open("./file/phones/txt", "r")
@@ -14,9 +14,9 @@ print(s)
 
 pfr.close()
 
-#반복문으로 파일에서 읽어서 모니터에 출력하기
+# 반복문으로 파일에서 읽어서 모니터에 출력하기
 print("반복문으로 파일에서 읽어서 모니터에 출력하기")
-pfr = open("./file/phone.txt", "r") 
+pfr = open("./file/phone.txt", "r")
 
 line = pfr.readline()
 while line != "":
